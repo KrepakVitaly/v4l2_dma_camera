aea89dc05c9e931260313772499c8785bd6b3f3e
krepak <krepakvitaliy@gmail.com>
2020-10-02 00:37:54 +0300

    add simple app with make

--- software/v4l2_dma_camera.c
+++ software/v4l2_dma_camera.c
@@ -1,10 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <fcntl.h>                /* low-level i/o */
+#include <unistd.h>
+#include <signal.h>
+#include <errno.h>
+#include <malloc.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/time.h>
+#include <sys/ioctl.h>
+#include <linux/videodev2.h>
+#include <pthread.h>
+#include <semaphore.h>
+#include <iostream>
+#include <fstream>
+#include <ctime>
+#include <stdint.h>
 
+static const char short_options[] = "d:hv:";
+static const struct option long_options[] = {
+    { "device",  required_argument, NULL, 'd' },
+    { "help",    no_argument,       NULL, 'h' },
+    { "video",   required_argument, NULL, 'v' },
+    { 0, 0, 0, 0 }
+};
 
+void usage(char* exec)
+{
+    printf("Usage: %s [options]\n"
+        "Options:\n"
+        "  -d | --device name       Use name as spidev device "
+        "(/dev/spidev0.1 by default)\n"
+        "  -h | --help              Print this message\n"
+        "  -v | --video name        Use name as v4l2loopback device "
+        "(%s by default)\n"
+        "", exec, v4l2dev);
+}
 
 
 int main(int argc, char **argv)
 {
+    // processing command line parameters
+    for (;;) {
+        int index;
+        int c;
 
+        c = getopt_long(argc, argv,
+            short_options, long_options,
+            &index);
 
+        if (-1 == c)
+            break;
+
+        switch (c) {
+        case 0:
+            break;
+
+        case 'd':
+            //spidev = optarg;
+            break;
+
+        case 'h':
+            usage(argv[0]);
+            exit(EXIT_SUCCESS);
+
+        case 'v':
+            v4l2dev = optarg;
+            break;
+
+        default:
+            usage(argv[0]);
+            exit(EXIT_FAILURE);
+        }
+    }
+
+    return 0;
+}
 
 
