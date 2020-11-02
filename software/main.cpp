bac40bcaa84b07219fe499658d3c40c148a219e7
krepak <krepakvitaliy@gmail.com>
2020-11-02 22:39:23 +0300

    fix

--- software/main.cpp
+++ software/main.cpp
@@ -14,20 +14,20 @@ int main(int argc, char** argv)
     // TODO check kill -9
     signal(SIGINT, sig_handler); // Register signal handler
 
-    open_vpipe();
+    //open_vpipe();
 
     uint16_t i = 0;
     while (1)
     {
         i++;
         printf("Start Send frame");
-        send_frame(0);
+        //send_frame(0);
 
         usleep(41000);
         printf("Frame %d\r\n", i);
     }
 
-    close_vpipe();
+    //close_vpipe();
 
     return 0;
 }
@@ -37,6 +37,6 @@ void sig_handler(int signum)
     // TODO check kill -9
     //Return type of the handler function should be void
     printf("\nInside handler function signum %d\n", signum);
-    close_vpipe();
+    //close_vpipe();
     exit(0);
 }
