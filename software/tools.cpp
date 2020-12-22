c81ad770d6a7fbbcd07a7e5ee09207a9ee333175
krepak <krepakvitaliy@gmail.com>
2020-12-22 17:55:49 +0300

    pix_8bit++;

--- software/tools.cpp
+++ software/tools.cpp
@@ -33,7 +33,7 @@ void reodrder_data_ir_camera_rggb(uint8_t* src, uint16_t src_w, uint16_t src_h, 
             pix_16bit_3 = src[raw * src_w * 4 + col * 4 + 3];
             pix_16bit = pix_16bit_3 + (pix_16bit_2 << 8);
 
-            pix_8bit = uint8_t(pix_16bit);
+            pix_8bit++;// = uint8_t(pix_16bit);
 
             //dest[raw * dest_w * 4 + col * 4 + 0] = pix_16bit_0;
             //dest[raw * dest_w * 4 + col * 4 + 1] = pix_16bit_1;
