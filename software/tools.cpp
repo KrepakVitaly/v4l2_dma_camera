17893fa7cfde56ce105c6a874a47bf54ae92806c
krepak <krepakvitaliy@gmail.com>
2020-12-22 18:53:56 +0300

    dest[raw * dest_w + col] = pix_8bit++;

--- software/tools.cpp
+++ software/tools.cpp
@@ -49,7 +49,7 @@ void reodrder_data_ir_camera_rggb(uint8_t* src, uint16_t src_w, uint16_t src_h, 
             //dest[raw * dest_w * 4 + col * 4 + 2] = pix_16bit_2;
             //dest[raw * dest_w * 4 + col * 4 + 3] = pix_16bit_3;
 
-            //dest[raw * dest_w + col] = pix_8bit;
+            dest[raw * dest_w + col] = pix_8bit++;
             if (0)
             {
                 printf("------\r\n");
